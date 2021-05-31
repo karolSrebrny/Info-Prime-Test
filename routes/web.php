d2e4c0d1e5b948d6fb3123bf1c4c9ca66104dcad
Vladimir Kabanets <karolsrebrny@gmail.com>
2021-06-01 00:58:40 +0300

    Fixed web.php file

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,5 +14,22 @@ use Illuminate\Support\Facades\Auth;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes(['register' => true]);
 
-Auth::routes(['register' => false]);
+Route::group(['middleware' => 'web'], function () {
+    Route::get('/', [ArticleController::class, 'index'])->name('home');
+
+    Route::group(['prefix' => 'articles', 'as' => 'articles.'], function () {
+        Route::get('/{article}', [ArticleController::class, 'show'])->name('show');
+    });
+
+    Route::group(['prefix' => 'admin', 'middleware' => 'auth', 'as' => 'admin.'], function () {
+        Route::group(['prefix' => 'articles', 'as' => 'article.'], function () {
+            Route::get('/create', [ArticleController::class, 'create'])->name('create');
+            Route::post('/store', [ArticleController::class, 'store'])->name('store');
+            Route::get('/edit/{article}', [ArticleController::class, 'edit'])->name('edit');
+            Route::put('/update/{article}', [ArticleController::class, 'update'])->name('update');
+            Route::delete('/delete/{article}', [ArticleController::class, 'destroy'])->name('delete');
+        });
+    });
+});
